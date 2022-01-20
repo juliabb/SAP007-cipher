40cac140af455c8d857d3f8af2fe0828c7c790e9
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-01-20 18:32:15 -0300

    Atualização HTML e JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,12 @@
+//cipher.encode(offset, string): offset é o número de posições que queremos mover para a direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
+
 const cipher = {
-  // ...
+  encode (offset_1, mensagemCrip){
+
+    let answerCifra = ""; // lugar onde vai aparecer a resposta
+
+    
+  }
 };
 
 export default cipher;
@@ -7,4 +14,7 @@ export default cipher;
 
 //Funções Cifrar e Decifrar
 
-function (cifrar)
+/*
+cipher.decode(offset, string): offset é o número de posições que queremos mover para a esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar. 
+
+*/
