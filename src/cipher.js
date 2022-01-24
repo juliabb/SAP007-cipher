97391509d8107d2e3a3b161dfb501d0ddd3be036
juliabb <juliabenedictohfvcjd@gmail.com>
2022-01-24 20:10:07 -0300

    Atualização Js cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,25 @@
+const alfabeto =[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+
 //cipher.encode(offset, string): offset é o número de posições que queremos mover para a direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
 
 const cipher = {
   encode (offset_1, mensagemCrip){
 
+    let input = document.getElementById("textCifra").value.toUpperCase(); //transformando o texto em MAIUSCULA - value é o value inserido pelo usuario
+
     let answerCifra = ""; // lugar onde vai aparecer a resposta
 
-    
+
+    /* Código ASC - A = 65 .... Z = 90  ((codigoDaLetraASC - cod1Letra + desloc) % tamAlfabeto) + cod1Letra
+
+    codigoASC => codigo0a25 => desloco => giro (loop) => codigoASC */
+
+    for (var i=0; i<input.length; i++){
+      let codigoAsc = 
+    }
+
   }
+  
 };
 
 export default cipher;
-
-
-//Funções Cifrar e Decifrar
-
-/*
-cipher.decode(offset, string): offset é o número de posições que queremos mover para a esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar. 
-
-*/
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
+
 //Interações (DOM, EventListener, document.getElement)
 
 //Pegando a interação com o botão
+function encode(){
 document.getElementById("cifrar").addEventListener('click', cifrar)
+
+}
+
+function decode (){
 document.getElementById("decifrar").addEventListener('click', decifrar)
+}
 
