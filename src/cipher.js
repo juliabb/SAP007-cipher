b4544f898dd2151afb6f27736393304679f22b81
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-01-26 19:00:28 -0300

    Atualização HTML, Css e lógica cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,35 @@
+
+const cipher = {
+  encode: function (offset,  mensagemCifra) {
+    
+    let text = ""; 
+    for (let i = 0; i < mensagemCifra.length; i++); {
+      const element = mensagemCifra[i];
+      let codigoAsc = element.charCodeAt(0);
+      let encodeValue = ((codigoAsc - 65 + offset))
+    }
+
+  },
+};
+
+export default cipher;
+
+
+
+/* let answerCifra = ""; // lugar onde vai aparecer a resposta
+let deslocamento = document.getElementById('offset_1');
+
+let quantiLetras = mensagemCifra.length; // lendo a quantidade de letra - length é leitura
+for (let i = 0; quantiLetras > i; i++) {
+  let codigoAsc = mensagemCifra.charCodeAt(i);
+  
+  let encodeValue =
+  ((codigoAsc - valorLetraAsc + deslocamento) % 26) + codigoAsc;
+  answerCifra = answerCifra.concat(mensagemCifra.fromCharCode(encodeValue));
+}
+*/
+
+
 /*    Código ASC - A = 65 .... Z = 90
         ((codigoDaLetraASC - cod1Letra + desloc) % tamAlfabeto) + cod1Letra
 
@@ -6,21 +38,3 @@
       cipher.encode(offset, string): offset é o número de posições que queremos mover para a direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
 
 */
-
-const cipher = {
-  encode(offset_1, mensagemCifra) {
-    let answerCifra = ""; // lugar onde vai aparecer a resposta
-    let deslocamento = document.getElementById('offset_1');
-
-    let quantiLetras = mensagemCifra.length; // lendo a quantidade de letra - length é leitura
-    for (let i = 0; quantiLetras > i; i++) {
-      let codigoAsc = mensagemCifra.charCodeAt(i); 
-      let encodeValue =
-        ((codigoAsc - valorLetraAsc + deslocamento) % 26) + codigoAsc;
-      answerCifra = answerCifra.concat(mensagemCifra.fromCharCode(encodeValue));
-    }
-    console.log(answerCifra);
-  },
-};
-
-export default cipher;
