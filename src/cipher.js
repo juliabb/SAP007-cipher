14f0be9bd9bff2330f3b51c22537878553522fb8
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-02-01 23:31:17 -0300

    lógica cifra funcionando, correção do layout e correção na semantica HTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,37 @@
-
 const cipher = {
-  encode (offset,  string) {
-    let text = ""; 
-    let quantiLetras = string.length
-    for (let i = 0; i < quantiLetras.length; i++) {
-      let codigoAsc = element.charCodeAt(i); //código asc 65 - 90
-      let encodeValue = ((codigoAsc - 65 + offset)) % 26 + 65; //lógica da cifra, aqui a letra está em 0 - 25
-      let text = text.concat(String.fromCharCode(encodeValue)); //aqui ele transforma em letra no metodo ASC 
+  encode: function (offset, string) {
 
+    let textCode = "";
+
+    for (let i = 0; i < string.length; i++) {
+      let cifrar = string.charCodeAt(i);
+      if (cifrar >= 65 && cifrar <= 90) {
+        textCode += String.fromCharCode(((cifrar - 65 + offset) % 26) + 65);
+      } else if (cifrar == 32) {
+        textCode += textCode.charAt(i);
+      }
     }
-      return text; 
-    console.log(text);
-  }
+    return textCode;
+  },
+
+  decode: function (offset, string) {
+
+    let textDecode = "";
+
+    for (let i = 0; i < string.length; i++) {
+      //laço para continuar enquanto ouver letras
+      let decifrar = string.charCodeAt(i);
+      if (decifrar >= 65 && decifrar <= 90) {
+        textDecode += String.fromCharCode(((decifrar - 65 - offset) % 26) + 65);
+      } else if (decifrar == 32) {
+        textDecode += textDecode.charAt(i);
+      }
+    }
+    return textDecode;
+  },
 };
 
 export default cipher;
-
-
 
 /* let answerCifra = ""; // lugar onde vai aparecer a resposta
 let deslocamento = document.getElementById('offset_1');
@@ -29,10 +44,8 @@ for (let i = 0; quantiLetras > i; i++) {
   ((codigoAsc - valorLetraAsc + deslocamento) % 26) + codigoAsc;
   answerCifra = answerCifra.concat(mensagemCifra.fromCharCode(encodeValue));
 }
-*/
 
-
-/*    Código ASC - A = 65 .... Z = 90
+Código ASC - A = 65 .... Z = 90
         ((codigoDaLetraASC - cod1Letra + desloc) % tamAlfabeto) + cod1Letra
 
         codigoASC => codigo0a25 => desloco => giro (loop) => codigoASC 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,33 @@ import cipher from './cipher.js';
 
 //Pegando a interação com o botão
 const botaoCifra = document.getElementById('bttnCifra');
-function bttnCifra (){
+
+function bttnCifra (e){
+
+     e.preventDefault() //impede o recarregamento auto da página
+     let offset = Number(document.getElementById('offset').value);
      let string = document.getElementById('text').value; //lendo o valor da textarea
-     let offset = parseInt(document.getElementById('offset').value);
 
      const resultadoCifra = cipher.encode(offset, string);
-     document.getElementById('answerCifra').innerHTML = resultadoCifra; //puxando a cifra do outro js e colocando o resultado no HTML
-}
 
-botaoCifra.addEventListener('click', bttnCifra);
+     document.getElementById('answers').innerHTML = resultadoCifra; //puxando a cifra do outro js e colocando o resultado no HTML
+}
+ 
+botaoCifra.addEventListener('click', bttnCifra) 
+
+//DECIFRA
+
+const botaoDecifra = document.getElementById('bttnDeci');
+
+function bttnDeci (e){
+
+     e.preventDefault() //impede o recarregamento auto da página
+     let offset = Number(document.getElementById('offset').value);
+     let string = document.getElementById('text').value; //lendo o valor da textarea
+
+     const resultadoDecifra = cipher.decode(offset, string);
+
+     document.getElementById('answers').innerHTML = resultadoDecifra; //puxando a cifra do outro js e colocando o resultado no HTML
+}
+ 
+botaoDecifra.addEventListener('click', bttnDeci);
