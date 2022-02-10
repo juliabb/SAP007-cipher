17e57bb20e9f9ebde76384a31e34d1fa7ce7ad0c
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-02-09 21:33:44 -0300

    Updates

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+//Interações (DOM, EventListener, document.getElement), innerHTML
+
 import cipher from './cipher.js';
 
-//Interações (DOM, EventListener, document.getElement)
-
-//Pegando a interação com o botão
+//Pegando a interação com o botão CIFRA
 const botaoCifra = document.getElementById('bttnCifra');
 
 function bttnCifra (e){
 
      e.preventDefault() //impede o recarregamento auto da página
-     let offset = Number(document.getElementById('offset').value);
-     let string = document.getElementById('text').value; //lendo o valor da textarea
+     let offset = Number(document.getElementById('offset').value); //lendo o valor do Numero do input
+     let string = document.getElementById('text').value; //lendo o valor string da textarea
 
-     const resultadoCifra = cipher.encode(offset, string);
+     const resultadoCifra = cipher.encode(offset, string); //puxando o resultado da função da cifra dentro cipher.js
 
-     document.getElementById('answers').innerHTML = resultadoCifra; //puxando a cifra do outro js e colocando o resultado no HTML
+     document.getElementById('answers').innerHTML = resultadoCifra; //definindo o lugar (answers que esta no index.html) e colocando o resultado no HTML (innerHTML)
 }
  
-botaoCifra.addEventListener('click', bttnCifra) 
+botaoCifra.addEventListener('click', bttnCifra) //chamando a função bttnCifra (linha 8) quando tiver o click no botão
 
-//DECIFRA
+//Pegando a interação com o botão DECIFRA
 
 const botaoDecifra = document.getElementById('bttnDeci');
 
@@ -26,11 +26,12 @@ function bttnDeci (e){
 
      e.preventDefault() //impede o recarregamento auto da página
      let offset = Number(document.getElementById('offset').value);
-     let string = document.getElementById('text').value; //lendo o valor da textarea
+     let string = document.getElementById('text').value; 
 
      const resultadoDecifra = cipher.decode(offset, string);
 
-     document.getElementById('answers').innerHTML = resultadoDecifra; //puxando a cifra do outro js e colocando o resultado no HTML
+     document.getElementById('answers').innerHTML = resultadoDecifra; 
 }
  
 botaoDecifra.addEventListener('click', bttnDeci);
+
