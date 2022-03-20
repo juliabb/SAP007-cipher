10ab5540db48cee50129198589e3beec2f53e67e
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-03-20 16:23:48 -0300

    Adicionando responsividade e função innerHTML para aparecer os resultados ao clicar nos botões

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
 import cipher from "./cipher.js";
 
+const buttonCipher = document.getElementById("buttonCipher");
+const buttonDecipher = document.getElementById("buttonDecipher");
 
-const botaoCifra = document.getElementById("buttonCipher");
-const botaoDecifra = document.getElementById("buttonDecipher");
+buttonCipher.addEventListener("click", cipherTextReturn);
+buttonDecipher.addEventListener("click", decipherTextReturn);
 
-botaoCifra.addEventListener("click", buttonCipher);
-botaoDecifra.addEventListener("click", buttonDecipher);
-
-function buttonCipher(e) {
+function cipherTextReturn(e) {
   e.preventDefault();
-  let offset = Number(document.getElementById("offset").value);
-  let string = document.getElementById("text-insert").value;
-  const resultadoCifra = cipher.encode(offset, string);
-  window.scrollBy(0, window.innerHeight);
+  const offset = Number(document.getElementById("offset").value)
+  const string = document.getElementById("text-insert").value
+  const resultCipher = cipher.encode(offset, string);
+  const shownResult = document.getElementById("result-show")
+  
+  let mensageError = 'Ops...Não foi possível realizar sua Cifragem! Por favor adicione uma Mensagem e um Deslocamento, e tente novamente.'
+    
+  window.scrollBy(0, window.innerHeight)
   if (offset,string == null && offset,string == undefined && offset,string == 0){
-    return alert('Necessário adicionar texto e deslocamento!! Verifique novamente.');
-  } else {
-  document.getElementById("result-show").innerHTML = resultadoCifra;
+    shownResult.classList.add('error');
+    return shownResult.innerHTML = mensageError;
+  }else {
+  shownResult.classList.remove('error')
+  shownResult.classList.add('result')
+  return shownResult.innerHTML = resultCipher;
   }
 }
 
-function buttonDecipher(e) {
+function decipherTextReturn(e) {
   e.preventDefault();
-  let offset = Number(document.getElementById("offset").value);
-  let string = document.getElementById("text-insert").value;
-  const resultadoDecifra = cipher.decode(offset, string);
-  window.scrollBy(0, window.innerHeight);
+  const offset = Number(document.getElementById("offset").value)
+  const string = document.getElementById("text-insert").value
+  const resultDecipher = cipher.decode(offset, string);
+  const shownResult = document.getElementById("result-show")
+  
+  let mensageError = 'Ops...Não foi possível realizar sua Decifragem! Por favor adicione a Mensagem Cifrada e o Deslocamento, e tente novamente.'
+    
+  window.scrollBy(0, window.innerHeight)
   if (offset,string == null && offset,string == undefined && offset,string == 0){
-    return alert('Necessário adicionar texto e deslocamento!! Verifique novamente.');
+    shownResult.classList.add('error');
+    return shownResult.innerHTML = mensageError;
   } else {
-  const shownResult = document.getElementById("result-show").innerHTML = resultadoDecifra;
-  return shownResult.classList.add("open");
+  shownResult.classList.remove('error')
+  shownResult.classList.add('result')
+  return shownResult.innerHTML = resultDecipher;
   }
 }
-
